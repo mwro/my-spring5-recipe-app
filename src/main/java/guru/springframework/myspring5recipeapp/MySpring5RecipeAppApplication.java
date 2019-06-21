47de2710f81d03b29f6630e1ddcacfd08cb9e2a8
Marcin <mwro91@gmail.com>
2019-06-21 09:53:35 +0200

    Initial commit

---
+++ src/main/java/guru/springframework/myspring5recipeapp/MySpring5RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.myspring5recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpring5RecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpring5RecipeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/myspring5recipeapp/MySpring5RecipeAppApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework.myspring5recipeapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MySpring5RecipeAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
